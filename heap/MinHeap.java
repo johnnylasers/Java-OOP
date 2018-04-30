1d73262fd7942bc3e8d2537142aad40b74cfe108
johnnylasers <jianchen85@gmail.com>
2018-04-30 14:52:32 -0700

    finished minHeap (integer) implementation

--- heap/MinHeap.java
+++ heap/MinHeap.java
@@ -1,5 +1,7 @@
 package OOP.heap;
 
+import java.util.Arrays;
+
 /**
  * for simplicity, implement a minHeap that support Integer first
  */
@@ -52,16 +54,46 @@ public class MinHeap {
         return heapArray[0];
     }
 
-    public boolean offer() {
-
+    public void offer(int e) {
+        if (size == heapArray.length) {
+            int[] oldArray = heapArray;
+            heapArray = new int[oldArray.length * 2];
+            heapArray = Arrays.copyOfRange(oldArray, 0, oldArray.length - 1);
+        }
+        heapArray[size] = e;
+        size++; // must increment the size before calling percolateUp
+                // b/c in percolateUp, the boundaries are based on the new size
+        percolateUp(size - 1);
     }
 
     public int poll() {
+        if (size == 0) {
+            throw new NullPointerException("Illegal operation. Heap is empty!");
+        }
+        int toReturn = heapArray[0];
+        heapArray[0] = heapArray[size - 1];
+        // or swap(heapArray, 0, size - 1);
+        size--;
+        percolateDown(0);
 
+        return toReturn;
     }
 
-    public boolean update() {
-
+    //return the original value
+    // update the element at target index with a new value
+    // don't forget to percolate
+    public int update(int targetIndex, int newValue) {
+        if (targetIndex < 0 || targetIndex >= heapArray.length) {
+            throw new ArrayIndexOutOfBoundsException("Target index out of bounds!");
+        }
+        int oldValue = heapArray[targetIndex];
+        heapArray[targetIndex] = newValue;
+        if (newValue > oldValue) {
+            percolateDown(targetIndex);
+        } else if (newValue < oldValue){
+            percolateUp(targetIndex);
+        }
+        return oldValue;
     }
 
     private void heapify() {
@@ -72,10 +104,39 @@ public class MinHeap {
 
     private void percolateDown(int index) {
 
+        while (index <= (size - 2) / 2) { // <==> 2 * index + 1 <= size - 1
+            int leftChildIndex = 2 * index + 1;
+            int rightChildIndex = 2 * index + 2;
+
+            //initialize swapCandidate with leftChildIndex
+            // at this point, we don't know yet if rightChildIndex is out of bounds
+            int swapCandidate = leftChildIndex;
+
+            if (rightChildIndex <= size - 1 && heapArray[leftChildIndex] > heapArray[rightChildIndex]) {
+                swapCandidate = rightChildIndex;
+            }
+
+            if (heapArray[index] > heapArray[swapCandidate]) {
+                swap(heapArray, index, swapCandidate);
+            } else {
+                break;
+            }
+
+            index = swapCandidate;
+        }
     }
 
     private void percolateUp(int index) {
+        while (index > 0) {
+            int parentIndex = (index - 1) / 2;
+            if (heapArray[index] < heapArray[parentIndex]) {
+                swap(heapArray, parentIndex, index);
 
+            } else {
+                break;
+            }
+            index = parentIndex;
+        }
     }
 
     private void swap(int[] array, int i, int j) {
